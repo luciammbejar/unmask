70efb36cb0f338a070a83e73783e5227dac5ba54
luciammbejar <luciammbejar@gmail.com>
2026-09-04 11:25:39 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Configuración pública de Unmask.
+// Esta clave es una "publishable key" y está pensada para usarse desde el navegador.
+const SUPABASE_URL = 'https://gdcnvsqtastnfgpmxlly.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_7GG1WDL0TMJY7bkLrjN3Fg_rD8HLXAm';
