e6df439966978a87db92074a2d473b4acc4fda69
luciammbejar <luciammbejar@gmail.com>
2026-09-04 11:25:27 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Configuración pública de Unmask.
-// Esta clave es una "publishable key" y está pensada para usarse desde el navegador.
-const SUPABASE_URL = 'https://gdcnvsqtastnfgpmxlly.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_7GG1WDL0TMJY7bkLrjN3Fg_rD8HLXAm';
